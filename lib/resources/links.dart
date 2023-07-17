90c42d57ad02dfaa682993ab5ded62bb809c30de
orl0pl <orlyyt@int.pl>
2023-07-17 09:36:37 +0200

    Revert "maybe fixed"
    This reverts commit f25c52c4dcbb34128a18a945b45b39095db54909.

--- lib/resources/links.dart
+++ lib/resources/links.dart
@@ -1,3 +1,3 @@
-const lemmonRepositoryUrl = 'https://github.com/orl0pl/lemmon';
+const lemmonRepositoryUrl = 'https://github.com/lemmon-app/lemmon';
 const markdownGuide =
     'https://join-lemmy.org/docs/en/about/guide.html#using-markdown';
